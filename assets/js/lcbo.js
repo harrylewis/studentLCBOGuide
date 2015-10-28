80ab7028f0db7ef57d3ff6f2995524b96cbe4d4b
Harry Lewis <harry.lewis@queensu.ca>
2015-10-28 13:57:37 -0400

    Store locator based on geopoint

--- assets/js/lcbo.js
+++ assets/js/lcbo.js
@@ -4,17 +4,43 @@ $(function() {
 	var SLCBOG = (function() {
 
 		// pull data from the LCBO object
-		$.ajax({
-			url: 'https://lcboapi.com/products?access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-			method: 'GET',
-			dataType: 'json',
-			headers: { 'Authorization': 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk' },
-			crossDomain	: true
-		}).then(function(data) {
-			for (var i = 0; i < data.result.length; i++)
-		  		console.log(data.result[i]);
-		});
+		// $.ajax({
+		// 	url: 'https://lcboapi.com/stores?lat=76.5000&lon=44.2333&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+		// 	method: 'GET',
+		// 	dataType: 'jsonp', 
+		// 	headers: {
+		// 		Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+		// 		Accept : 'application/vnd.api+json'
+		// 	},
+		// 	crossDomain	: true
+		// }).then(function(data) {
+		// 	console.log(data);
+		// 	for (var i = 0; i < data.result.length; i++)
+		//   		console.log(data.result[i].name);
+		// });
 
-	});
+		navigator.geolocation.getCurrentPosition(showPosition);
+
+		function showPosition(position) {
+			console.log(position);
+			console.log(position.coords.latitude);
+			console.log(position.coords.longitude);
+			$.ajax({
+				url: 'https://lcboapi.com/stores?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+				method: 'GET',
+				dataType: 'jsonp', 
+				headers: {
+					Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+					Accept : 'application/vnd.api+json'
+				},
+				crossDomain	: true
+			}).then(function(data) {
+				console.log(data);
+				for (var i = 0; i < data.result.length; i++)
+			  		console.log(data.result[i].name);
+			});
+		}
+
+	})();
 					
 });
