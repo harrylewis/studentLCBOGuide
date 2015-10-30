ac9dcd3bac43d5cdb432caa2d608bad56973ece5
Harry Lewis <harry.lewis@queensu.ca>
2015-10-30 13:48:11 -0400

    Geolocation added

--- assets/js/lcbo.js
+++ assets/js/lcbo.js
@@ -37,7 +37,24 @@ $(function() {
 			}).then(function(data) {
 				console.log(data);
 				for (var i = 0; i < data.result.length; i++)
-			  		console.log(data.result[i].name);
+			  		console.log(data.result[i]);
+			  	// find beer at a place
+			  	$.ajax({
+					url: 'https://lcboapi.com/products?store=' + data.result[0].id + '&q=coors+light&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+					method: 'GET',
+					dataType: 'jsonp', 
+					headers: {
+						Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+						Accept : 'application/vnd.api+json'
+					},
+					crossDomain	: true
+				}).then(function(data) {
+					console.log(data);
+					for (var i = 0; i < data.result.length; i++)
+				  		console.log(data.result[i]);
+				  	// find beer at a place
+				  	
+				});
 			});
 		}
 
