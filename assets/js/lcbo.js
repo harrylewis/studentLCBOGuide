bc672f58da8ceb43528e8814c1eb7d220d637097
Harry Lewis <harry.lewis@queensu.ca>
2015-10-30 22:04:10 -0400

    Readded files

---
+++ assets/js/lcbo.js
@@ -0,0 +1,77 @@
+$(function() {
+	
+	// global app object
+	var SLCBOG = (function() {
+
+		// pull data from the LCBO object
+		// $.ajax({
+		// 	url: 'https://lcboapi.com/stores?lat=76.5000&lon=44.2333&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+		// 	method: 'GET',
+		// 	dataType: 'jsonp', 
+		// 	headers: {
+		// 		Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+		// 		Accept : 'application/vnd.api+json'
+		// 	},
+		// 	crossDomain	: true
+		// }).then(function(data) {
+		// 	console.log(data);
+		// 	for (var i = 0; i < data.result.length; i++)
+		//   		console.log(data.result[i].name);
+		// });
+
+		// navigator.geolocation.getCurrentPosition(showPosition);
+
+		function showPosition(position) {
+			console.log(position);
+			console.log(position.coords.latitude);
+			console.log(position.coords.longitude);
+			$.ajax({
+				url: 'https://lcboapi.com/stores?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&page=5&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+				method: 'GET',
+				dataType: 'jsonp', 
+				headers: {
+					Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+					Accept : 'application/vnd.api+json'
+				},
+				crossDomain	: true
+			}).then(function(data) {
+				console.log(data);
+				console.log(data.result.length);
+				for (var i = 0; i < data.result.length; i++)
+<<<<<<< HEAD
+			  		console.log(data.result[i].updated_at);
+			  	// find beer at a place
+			  	$.ajax({
+					url: 'https://lcboapi.com/products?store=' + data.result[0].id + '&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+=======
+			  		console.log(data.result[i]);
+			  	console.log(data.result.length);
+			  	// find some deals
+			  	$.ajax({
+					url: 'https://lcboapi.com/products?store=' + data.result[0].id + '&q=vodka&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+>>>>>>> a0efe04287c10f85c81ab8ed8902e4bac1c01e4c
+					method: 'GET',
+					dataType: 'jsonp', 
+					headers: {
+						Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+						Accept : 'application/vnd.api+json'
+					},
+					crossDomain	: true
+				}).then(function(data) {
+					//console.log(data);
+					for (var i = 0; i < data.result.length; i++)
+				  		console.log(data.result[i]);
+				  	console.log(data.result.length);
+				  	// find beer at a place
+				  	
+				});
+			});
+		}
+
+		function getBeers(product) {
+
+		}
+
+	})();
+					
+});
