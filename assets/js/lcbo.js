f5f5b6d2776ca7a1f454b21bc41d372350bf4d0e
Evan Cloutier <12ec49@queensu.ca>
2015-11-02 14:39:09 -0500

    Cleaning up...

--- assets/js/lcbo.js
+++ assets/js/lcbo.js
@@ -16,64 +16,6 @@ $(function() {
 			getNearestStores(latitude, longitude, $('#searchProduct').val());
 		});
 
-		// pull data from the LCBO object
-		// $.ajax({
-		// 	url: 'https://lcboapi.com/stores?lat=76.5000&lon=44.2333&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-		// 	method: 'GET',
-		// 	dataType: 'jsonp', 
-		// 	headers: {
-		// 		Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-		// 		Accept : 'application/vnd.api+json'
-		// 	},
-		// 	crossDomain	: true
-		// }).then(function(data) {
-		// 	console.log(data);
-		// 	for (var i = 0; i < data.result.length; i++)
-		//   		console.log(data.result[i].name);
-		// });
-
-		//navigator.geolocation.getCurrentPosition(getNearestStores);
-
-		// function showPosition(position) {
-		// 	console.log(position);
-		// 	console.log(position.coords.latitude);
-		// 	console.log(position.coords.longitude);
-		// 	$.ajax({
-		// 		url: 'https://lcboapi.com/stores?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&page=5&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-		// 		method: 'GET',
-		// 		dataType: 'jsonp', 
-		// 		headers: {
-		// 			Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-		// 			Accept : 'application/vnd.api+json'
-		// 		},
-		// 		crossDomain	: true
-		// 	}).then(function(data) {
-		// 		console.log(data);
-		// 		console.log(data.result.length);
-		// 		for (var i = 0; i < data.result.length; i++)
-		// 	  		console.log(data.result[i]);
-		// 	  	console.log(data.result.length);
-		// 	  	// find some deals
-		// 	  	$.ajax({
-		// 			url: 'https://lcboapi.com/products?store=' + data.result[0].id + '&q=vodka&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-		// 			method: 'GET',
-		// 			dataType: 'jsonp', 
-		// 			headers: {
-		// 				Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-		// 				Accept : 'application/vnd.api+json'
-		// 			},
-		// 			crossDomain	: true
-		// 		}).then(function(data) {
-		// 			//console.log(data);
-		// 			for (var i = 0; i < data.result.length; i++)
-		// 		  		console.log(data.result[i]);
-		// 		  	console.log(data.result.length);
-		// 		  	// find beer at a place
-				  	
-		// 		});
-		// 	});
-		// }
-
 		function getNearestStores(latitude, longitude, queryResult) {
 			var selectedStores = [];
 			var distance = 5000;
