d8820e0778b2579d5513954f20421a4738c997fe
Evan Cloutier <12ec49@queensu.ca>
2015-11-03 15:18:30 -0500

    Added checkDeals but w/ error

--- assets/js/lcbo.js
+++ assets/js/lcbo.js
@@ -5,19 +5,30 @@ $(function() {
 		// global position variables for geolocation
 		var latitude;
 		var longitude;
+		// global variable to access nearest stores to user
+		var selectedStores = [];
 
 		// let's find out where you are
-		navigator.geolocation.getCurrentPosition(setCoordinates);
+		navigator.geolocation.getCurrentPosition(findStores);
 
 		// handle searching for alcohol
 		$('#alcoholSearch').submit(function(e) {
 			e.preventDefault();
+			
 			// get the user's current location to find the nearest stores
-			getNearestStores(latitude, longitude, $('#searchProduct').val());
+			var queryResult = $('#searchProduct').val();
+			getBeers(selectedStores, queryResult);
+			checkDeals(selectedStores);
 		});
 
-		function getNearestStores(latitude, longitude, queryResult) {
-			var selectedStores = [];
+		function findStores(position) {
+			// first we need to set the coordinates of the user
+			latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
+			console.log(latitude);
+			console.log(longitude);
+
+			// now we can find the nearest store within a radius
 			var distance = 5000;
 
 			$.ajax({
@@ -26,12 +37,12 @@ $(function() {
 				dataType: 'jsonp',
 				headers: {
 					Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-					Accept : 'application/vnd.api+json'
+					Accept: 'application/vnd.api+json'
 				},
-				crossDomain : true
+				crossDomain: true
 			}).then(function(data) {
 				// display stores
-				console.log(data);
+				// console.log(data);
 				for (var i = 0; i < data.result.length; i++) {
 					if (!data.result[i].is_dead && data.result[i].distance_in_meters < distance)
 						selectedStores.push(data.result[i]);
@@ -39,8 +50,6 @@ $(function() {
 
 				for (var i = 0; i < selectedStores.length; i++)
 					console.log(selectedStores[i]);
-
-				getBeers(selectedStores, queryResult);
 			});
 		}
 
@@ -51,21 +60,30 @@ $(function() {
 				dataType: 'jsonp',
 				headers: {
 					Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
-					Accept : 'application/vnd.api+json'
+					Accept: 'application/vnd.api+json'
 				},
-				crossDomain : true
+				crossDomain: true
 			}).then(function(products) {
-				console.log(products)
+				console.log(products);
 			});
 		}
 
-		function setCoordinates(position) {
-			latitude = position.coords.latitude;
-			longitude = position.coords.longitude;
-			console.log(latitude);
-			console.log(longitude);
+
+		function checkDeals(stores) {
+			$.ajax({
+				url: 'https://lcboapi.com/products?store=' + stores[0].id + '&where=has_limited_time_offer&access_key=MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+				method: 'GET',
+				dataType: 'jsonp',
+				headers: {
+					Authorization: 'MDo5ODdkZTJlNC03OGVmLTExZTUtYmFiNC0wM2FkNTRkMjcwOWM6U1pJczR0N2E0VTh0eUFWSVB4ZXFKeGdNblA4V3ZYd041YURk',
+					Accept: 'application/vnd.api+json'	
+				},
+				crossDomain: true
+			}).then(function(deals) {
+				console.log(deals);
+			});
 		}
 
-	})();
-					
+	});
+
 });
